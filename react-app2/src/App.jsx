913fa393861a6f1c288024ea75e509f623be4c6f
manish <manishkumarji85635@gmail.com>
2026-01-11 07:05:21 +0530

    we understand folder staructure new topic component kisi website ke har ek section ko chote chote component me todna

---
+++ react-app2/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from './components/Nav.jsx'
+import Footer from './components/Footer.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='lalka'>Hello Bhau</h1>
+
+      <nav><Nav /></nav>
+
+      <footer><Footer /></footer>
+
+    </div>
+  )
+}
+
+export default App
---
+++ react-app2/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <h1 className="nav">Hello Nav Bhau</h1>
+    </div>
+  )
+}
+
+export default Nav
