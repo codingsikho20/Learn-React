656d07fe83527dee9db36ace6f5b2ca080eece28
manish <manishkumarji85635@gmail.com>
2026-01-13 07:19:41 +0530

    learn tailwind css

---
+++ react-app3/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './components/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <nav><Nav /></nav>
+    </div>
+  )
+}
+
+export default App
