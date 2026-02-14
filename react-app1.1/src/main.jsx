fe086da6bbdfe5884a2d5c60d03490ff3fcd9485
manish <manishkumarji85635@gmail.com>
2026-02-14 07:54:53 +0530

    Task

---
+++ react-app1.1/src/main.jsx
@@ -0,0 +1,9 @@
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import './index.css'
+import React from 'react'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+  root.render(
+    <App />
+  )
